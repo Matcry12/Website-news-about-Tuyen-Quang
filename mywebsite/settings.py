04b769687ab2dccdc305f0d20d2460c59c328b42
Matcry12 <fdtywadw@gmail.com>
2024-12-26 12:51:21 +0700

    update code

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-v+v$e=9waxf(u6wr7l!3pf@_kf0s1tty#!y2$b=u@iuz&gz=4+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '4bfb-116-107-21-207.ngrok-free.app']
-CSRF_TRUSTED_ORIGINS = ['https://4bfb-116-107-21-207.ngrok-free.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '35.197.148.85']
 
 # Application definition
 
@@ -138,6 +137,8 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
